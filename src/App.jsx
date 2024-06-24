8238550f307f782d8832ba8c540a1bf1c6042675
Vladislav Sidorik <Vladislavs.Sidoriks@gmail.com>
2024-06-24 16:14:40 +0300

    added project stuff

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Text } from '@chakra-ui/react'
+
+import WelcomeSection from './page/WelcomeBlock/WelcomeSection.jsx'
+
+function App() {
+
+  return (
+    <>
+      <WelcomeSection/>
+    </>
+  )
+}
+
+export default App
